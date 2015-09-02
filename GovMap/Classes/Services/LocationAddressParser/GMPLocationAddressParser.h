896acef3d3baafbc4d7c2dc92bd581bdc9dca46b
Myroslava Polovka <Myroslava@Myroslavas-MacBook-Pro.local>
2015-09-02 13:57:42 +0300

    Location Address changes

---
+++ GovMap/Classes/Services/LocationAddressParser/GMPLocationAddressParser.h
@@ -0,0 +1,15 @@
+//
+//  GMPLocationAddressParser.h
+//  GovMap
+//
+//  Created by Myroslava Polovka on 9/2/15.
+//  Copyright (c) 2015 ThinkMobiles. All rights reserved.
+//
+
+@class GMPLocationAddress;
+
+@interface GMPLocationAddressParser : NSObject
+
++ (GMPLocationAddress *)locationAddressWithString:(NSString *)address;
+
+@end
