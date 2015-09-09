e4ae65f92e999dfead11e2955b847f938501a15b
Myroslava Polovka <Myroslava@Myroslavas-MacBook-Pro.local>
2015-09-09 09:55:26 +0300

    design changes

--- GovMap/GMPBorderedTextField.m
+++ GovMap/GMPBorderedTextField.m
@@ -38,8 +38,12 @@ static CGFloat kLeftViewWidth = 10.f;
     [self addRightView];
     
     self.textAlignment = NSTextAlignmentNatural;
-    self.backgroundColor = [UIColor whiteColor];
+    
     self.layer.cornerRadius  = 5.0f;
+    self.layer.masksToBounds = YES;
+    self.layer.borderColor   = UIColorFromRGB(0x03a9f4).CGColor;
+    self.layer.borderWidth   = 0.5f;
+    self.layer.cornerRadius  = 5.f;
 }
 
 - (void)addLeftView
